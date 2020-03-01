a8d2ad0c3810bbf8fb0062c532da436070a5b45d
nicopaepke <npaepke@gmx.net>
2020-03-01 20:18:19 +0100

    initial spring boot app

---
+++ src/main/java/npaepke/haushaltsbuch/HaushaltsbuchApplication.java
@@ -0,0 +1,13 @@
+package npaepke.haushaltsbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaushaltsbuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaushaltsbuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/npaepke/haushaltsbuch/HaushaltsbuchApplicationTests.java
@@ -0,0 +1,13 @@
+package npaepke.haushaltsbuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaushaltsbuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
